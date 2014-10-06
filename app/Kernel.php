7ee4c2267666cab88c8af5a49f0ad4c6ab04b606
Jakub Kanclerz <jakub.kanclerz@gmail.com>
2014-10-06 22:35:21 +0200

    handle routes collections

--- app/Kernel.php
+++ app/Kernel.php
@@ -2,10 +2,30 @@
 
 class Kernel
 {
+    protected $view;
+    protected $routes;
+
+    public function __construct($view, $routes)
+    {
+        $this->view = $view;
+        $this->routes = $routes;
+    }
+
     public function handle($request)
     {
-        if (!array_key_exists($request->query, 'page')) {
-            throw new InvalidArgumentException('You should provide "page" parameter like ?page=some-page', 404);
+        if (!array_key_exists('page', $request->query)) {
+            throw new InvalidArgumentException('You should provide "page" parameter like ?page=some-page', 401);
         }
+
+        $this->matchRoute($request->query['page']);
+    }
+
+    protected function matchRoute($page)
+    {
+        if (!array_key_exists($page, $this->routes)) {
+            throw new InvalidArgumentException(sprintf('Page %s was not found', $page), 404);
+        }
+
+        die('works');
     }
 }
--- app/app.php
+++ app/app.php
@@ -4,11 +4,15 @@ require_once __DIR__.'/Request.php';
 require_once __DIR__.'/Kernel.php';
 require_once __DIR__.'/HtmlView.php';
 
+$routesCollection = array(
+    'students-list' => 'listAction',
+);
+
 
 $request = Request::createFromGlobals();
 $htmlView = new HtmlView();
 
-$kernel = new Kernel($htmlView);
+$kernel = new Kernel($htmlView, $routesCollection);
 try {
     $kernel->handle($request);
 } catch (\Exception $e) {
